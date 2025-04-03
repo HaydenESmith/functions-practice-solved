a6572f456e3edf167c4bceac52a8e4c302573bcf
Hayden Smith <ha.smith623@gmail.com>
2025-04-03 01:32:50 -0500

    Solved e2.j and e3.j

--- exercises/e2.js
+++ exercises/e2.js
@@ -42,7 +42,7 @@ function totalRoomsArea(area1, area2, area3) {
 // Make the function call in the parentheses of the console.log statement.
 // Use room1, room2, room3 variables as arguments for the function call
 const result = function () {
-  console.log(totalRoomsArea());
+  console.log(totalRoomsArea(room1, room2, room3));
 };
 
 // === TEST YOURSELF ===
